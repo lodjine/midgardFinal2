7ab314c3582237ddb61ae17bbb91cae76c335b15
cdridi <cdridi@cdridi-adam.neylingservices.tn>
2017-10-07 10:19:01 +0200

    c

---
+++ src/main/resources/static/page/home/homeController.js
@@ -0,0 +1,16 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+(function() {
+	'use strict';
+
+	angular.module('midgApp').controller('homeController', homeController);
+	homeController.$inject = [ '$scope', '$state', '$rootScope', 'userService', '$q' ,'roleService'];
+
+	function homeController($scope, $state, $rootScope, userService,$q,roleService) {
+		var login = localStorage.getItem("login"); 
+		console.log(login);
+		$rootScope.userConect=userService.userByLogin({login:login});
+	}
+})();
